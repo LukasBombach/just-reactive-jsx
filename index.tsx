438fad16938b099da7a4f823e428e551adf71ff9
Lukas Bombach <lukas.bombach@googlemail.com>
2023-11-05 08:48:26 +0100

    wip

--- index.tsx
+++ index.tsx
@@ -1,8 +1,45 @@
 import { signal, effect } from "@maverick-js/signals";
+import * as pretty from "pretty-format";
 import enableJsDomGlobally from "jsdom-global";
 
 enableJsDomGlobally();
 
+interface JsxElement {
+  type: keyof JSX.IntrinsicElements;
+  props: Record<string, any>;
+}
+
+function print(...val: unknown[]) {
+  const options = { plugins: [pretty.plugins.DOMElement], highlight: true };
+  console.debug(...val.map(v => pretty.format(v, options)));
+}
+
 const $text = signal("Hello World");
 
-effect(() => console.log($text()));
+const jsx: JsxElement = {
+  type: "p",
+  props: {
+    children: $text,
+  },
+};
+
+function render(element: JsxElement) {
+  const { type, props } = element;
+  const el = document.createElement(type);
+  if (props.children) {
+    effect(() => {
+      let children = props.children();
+      console.debug("render", children);
+      el.textContent = children;
+    });
+  }
+  return el;
+}
+
+const el = render(jsx);
+
+print("before", el.textContent);
+
+$text.set("Brave new world");
+
+print("after", el.textContent);
