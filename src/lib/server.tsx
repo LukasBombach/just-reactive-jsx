f7655117b643934b9133c47dea2862840fdb3ae1
Lukas Bombach <lukas.bombach@googlemail.com>
2023-11-12 23:55:23 +0100

    code replacement to signal(0) works

--- src/lib/server.tsx
+++ src/lib/server.tsx
@@ -1,15 +1,98 @@
-import { parse } from "@swc/core";
+import { parse, print } from "@swc/core";
 import { Visitor } from "@swc/core/Visitor";
 
-import type { JSXAttrValue } from "@swc/types";
+import type {
+  Module,
+  Span,
+  JSXAttrValue,
+  VariableDeclaration,
+  VariableDeclarator,
+  Expression,
+  CallExpression,
+  Identifier,
+} from "@swc/types";
 
-class MyVisitor extends Visitor {
-  visitJSXAttributeValue(value: JSXAttrValue | undefined) {
-    console.log(JSON.stringify(value) + "\n");
+// Verwenden Sie einen Dummy-Span, wenn keine spezifische Positionierung erforderlich ist
+const dummySpan: Span = {
+  start: 0,
+  end: 0,
+  ctxt: 0,
+};
 
-    return value;
+function findIdentifiersInJsxAttributes(ast: Module): Identifier[] {
+  const identifiers: Identifier[] = [];
+
+  class FindIdentifiers extends Visitor {
+    visitJSXAttributeValue(value: JSXAttrValue | undefined) {
+      if (value?.type === "JSXExpressionContainer" && value.expression.type === "Identifier") {
+        identifiers.push(value.expression);
+      }
+
+      return value;
+    }
   }
+
+  class ModifyVariableVisitor extends Visitor {
+    visitVariableDeclaration(variableDeclaration: VariableDeclaration): VariableDeclaration {
+      if (variableDeclaration.declarations.length === 1) {
+        const declarator = variableDeclaration.declarations[0];
+        if (declarator.id.type === "Identifier" && declarator.id.value === "count") {
+          // Erstelle einen neuen VariableDeclarator für `const count = signal(0);`
+          const newDeclarator: VariableDeclarator = {
+            ...declarator,
+            id: declarator.id, // Behält den bestehenden Identifier bei
+            init: this.createSignalCallExpression(declarator.init), // Erstellt einen CallExpression für `signal(0)`
+          };
+
+          // Erstelle einen neuen VariableDeclaration-Knoten mit dem neuen Declarator
+          return {
+            ...variableDeclaration,
+            kind: "const", // Ändere 'let' zu 'const'
+            declarations: [newDeclarator],
+          };
+        }
+      }
+      return variableDeclaration;
+    }
+
+    createSignalCallExpression(originalInit: Expression | undefined): CallExpression {
+      // Erstellt eine CallExpression für `signal(0)`
+      return {
+        type: "CallExpression",
+        span: dummySpan,
+        callee: { type: "Identifier", span: dummySpan, value: "signal", optional: false },
+        arguments: originalInit ? [{ expression: { ...originalInit } }] : [],
+      };
+    }
+  }
+
+  new FindIdentifiers().visitProgram(ast);
+  new ModifyVariableVisitor().visitProgram(ast);
+
+  return identifiers;
 }
+
+/* function findVariableDeclaration(ast: Module, id: string): VariableDeclaration {
+  let declaration: VariableDeclaration | undefined;
+
+  class MyVisitor extends Visitor {
+    visitVariableDeclaration(d: VariableDeclaration) {
+      if (d.declarations[0].id.type === "Identifier" && d.declarations[0].id.value === id) {
+        declaration = d;
+      }
+
+      return d;
+    }
+  }
+
+  new MyVisitor().visitProgram(ast);
+
+  if (!declaration) {
+    throw new Error(`variable declaration for ${id} not found`);
+  }
+
+  return declaration;
+} */
 
 Bun.serve({
   port: 3000,
@@ -38,7 +121,18 @@ Bun.serve({
 
               const ast = await parse(contents, { syntax: "typescript", tsx: true });
 
-              new MyVisitor().visitProgram(ast);
+              const identifiers = findIdentifiersInJsxAttributes(ast);
+
+              const { code: transformedCode } = await print(ast);
+
+              console.log(`${transformedCode}`);
+
+              /* console.log(
+                identifiers
+                  .map(ident => findVariableDeclaration(ast, ident.value))
+                  .map(ident => JSON.stringify(ident, null, 2))
+                  .join("\n")
+              ); */
 
               return { contents };
             });
@@ -61,7 +155,7 @@ Bun.serve({
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>${path}</title>
+          <title>HERE ${path}</title>
         </head>
         <body>
           ${scripts}
