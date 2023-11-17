d690adee195b56a082c96f5a60bc6d119c6e6928
Lukas Bombach <lukas.bombach@googlemail.com>
2023-11-17 12:22:16 +0100

    wip

--- src/pages/augmented/counter.tsx
+++ src/pages/augmented/counter.tsx
@@ -1,10 +1,15 @@
+const A = () => <span>A</span>;
+const B = () => <span>B</span>;
+
 export default function Counter() {
   let count = 0;
 
   return (
     <div>
       <input value={count} />
-      <button onClick={() => (count = count + 1)}>count {count}</button>
+      <button onClick={() => (count = count + 1)}>count</button>
+      <br />
+      {count % 2 === 0 ? <A /> : <B />}
     </div>
   );
 }
