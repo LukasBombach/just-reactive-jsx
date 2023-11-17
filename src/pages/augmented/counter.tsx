4b8bb967e9ddda8900b6eb86cb1348cf3b4aa68b
Lukas Bombach <lukas.bombach@googlemail.com>
2023-11-17 14:42:11 +0100

    resetted the counter example

--- src/pages/augmented/counter.tsx
+++ src/pages/augmented/counter.tsx
@@ -1,24 +1,10 @@
-const A = () => <span>A</span>;
-const B = () => <span>B</span>;
-
 export default function Counter() {
   let count = 0;
 
   return (
-    <div>
+    <main>
       <input value={count} />
       <button onClick={() => (count = count + 1)}>count</button>
-      <br />
-      {count % 2 === 0 ? (
-        <A />
-      ) : (
-        <div>
-          <p>
-            <B />
-          </p>
-          <img src="https://www.t-online.de/s/paper/_next/static/media/t-online-desktop.0a5256e3.svg" />
-        </div>
-      )}
-    </div>
+    </main>
   );
 }
