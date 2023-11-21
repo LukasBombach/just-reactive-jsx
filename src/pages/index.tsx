061bfb7259b430531159f0bfc41e6be3e8dfa910
Lukas Bombach <lukas.bombach@googlemail.com>
2023-11-21 13:24:12 +0100

    text shadows!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,25 @@ export default function Home() {
   return (
     <main className="max-w-prose ml-24 mt-16 p-4">
       <header>
-        <h1>
-          <span className="block font-serif leading-[0.8em] text-6xl">Unfinished</span>
-          <span className="block font-serif leading-[0.8em] text-8xl">Thought</span>
+        <h1 className="">
+          <span className="block font-serif leading-[0.8em] [text-shadow:_1px_1px_2px_rgba(10,11,13,.8)] text-6xl">
+            Unfinished
+          </span>
+          <span className="block font-serif leading-[0.8em] [text-shadow:_1px_1px_2px_rgba(10,11,13,.8)] text-8xl">
+            Thought
+          </span>
         </h1>
       </header>
 
       <section className="mt-24">
-        <header className="uppercase text-sm text-[#13CE66]">Articles</header>
+        <header className="uppercase text-sm text-[#13CE66] [text-shadow:_1px_1px_2px_rgba(10,11,13,.1)]">
+          Articles
+        </header>
         <a className="block mt-4" href="/syntax-highlighting-with-css-and-a-background-image">
-          <h2 className="text-2xl font-bold">Syntax highlighting with CSS and a background image</h2>
-          <p className="font-light mt-2">
+          <h2 className="text-2xl font-bold [text-shadow:_1px_1px_2px_rgba(10,11,13,.4)]">
+            Syntax highlighting with CSS and a background image
+          </h2>
+          <p className="font-light mt-2 [text-shadow:_1px_1px_2px_rgba(10,11,13,.2)]">
             Syntax highlighters generate a ton of markup. This is a proof of concept for using a background image to
             highlight code instead.
           </p>
