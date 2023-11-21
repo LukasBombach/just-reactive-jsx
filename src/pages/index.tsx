43765fe49397f1aa37a1df6f1c9224a27c775e5b
Lukas Bombach <lukas.bombach@googlemail.com>
2023-11-21 12:45:35 +0100

    first article

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,23 @@
 export default function Home() {
   return (
-    <main className="max-w-screen-md ml-24 mt-12 p-4">
-      <h1 className="">
-        <span className="block font-serif leading-[0.8em] text-6xl">Unfinished</span>
-        <span className="block font-serif leading-[0.8em] text-8xl">Thought</span>
-      </h1>
-      <ul>
-        <li>
-          <a href="./counter">Counter</a>
-        </li>
-        <li>
-          <a href="./tailwind">Tailwind</a>
-        </li>
-        <li>
-          <a href="./SplitScreen">SplitScreen</a>
-        </li>
-      </ul>
+    <main className="max-w-prose ml-24 mt-16 p-4">
+      <header>
+        <h1>
+          <span className="block font-serif leading-[0.8em] text-6xl">Unfinished</span>
+          <span className="block font-serif leading-[0.8em] text-8xl">Thought</span>
+        </h1>
+      </header>
+
+      <section className="mt-24">
+        <header className="uppercase text-sm text-[#13CE66]">Articles</header>
+        <a className="block mt-4" href="/syntax-highlighting-with-css-and-a-background-image">
+          <h2 className="text-2xl font-bold">Syntax highlighting with CSS and a background image</h2>
+          <p className="font-light mt-2">
+            Syntax highlighters generate a ton of markup. This is a proof of concept for using a background image to
+            highlight code instead.
+          </p>
+        </a>
+      </section>
     </main>
   );
 }
