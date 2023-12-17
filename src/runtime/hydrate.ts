6edc8a8b55f7af6f004091728c914658027784bc
Lukas Bombach <lukas.bombach@googlemail.com>
2023-12-17 11:13:30 +0100

    types and stuff

--- src/runtime/hydrate.ts
+++ src/runtime/hydrate.ts
@@ -4,18 +4,10 @@ import { event } from "./event";
 import { child } from "./child";
 
 type Hydration = [number, "attr" | "event" | "child", string, any];
+type HydrationFn<D> = (_props: null, initialData: D) => Hydration[];
 
-function Counter(ssrData: any[]) {
-  const count = signal(ssrData[0]);
-
-  return [
-    [0, "attr", "value", count],
-    [1, "event", "click", () => count.set(count() + 1)],
-    [2, "child", "", count],
-  ];
-}
-export function hydrate<D>(Component: (data: D) => Hydration[], ssrData: D) {
-  const hydrate = Component(ssrData);
+export function hydrate<T>(Component: HydrationFn<T>, initialData: T) {
+  const hydrate = Component(null, initialData);
   for (const [id, type, name, value] of hydrate) {
     const el = document.querySelector(`r\\:${id}`) as HTMLElement;
 
@@ -31,3 +23,15 @@ export function hydrate<D>(Component: (data: D) => Hydration[], ssrData: D) {
     }
   }
 }
+
+const Counter: HydrationFn<[number]> = (_props, initialData) => {
+  const count = signal(initialData[0]);
+
+  return [
+    [0, "attr", "value", count],
+    [1, "event", "click", () => count.set(count() + 1)],
+    [2, "child", "", count],
+  ];
+};
+
+hydrate(Counter, [0]);
