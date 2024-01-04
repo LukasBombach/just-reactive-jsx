2482b8224f80df8840418418aef568fc06f48c4c
Lukas Bombach <lukas.bombach@googlemail.com>
2024-01-04 21:45:05 +0100

    cli for dev server

---
+++ cli/index.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { startDevServer } from "../server/development";
+
+const CliSchema = z.object({
+  command: z.enum(["dev"]),
+});
+
+function parseArgs(args: string[]) {
+  try {
+    const parsedArgs = CliSchema.parse({ command: args[2] });
+    return parsedArgs;
+  } catch (error) {
+    console.error("Invalid arguments:", error);
+    process.exit(1);
+  }
+}
+
+const args = parseArgs(process.argv);
+
+if (args.command === "dev") {
+  startDevServer();
+} else {
+  console.log("Command not recognized.");
+}
