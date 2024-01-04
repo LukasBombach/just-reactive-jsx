436f369a96f69611c7ba4cc5c3d7225fabf04971
Lukas Bombach <lukas.bombach@googlemail.com>
2024-01-04 23:35:46 +0100

    bs render to string

--- renderer/getHtmlResponse.ts
+++ renderer/getHtmlResponse.ts
@@ -1,7 +1,9 @@
+import { renderToString } from "../src/server/renderToString";
+
 export async function getHtmlResponse(path: string): Promise<string> {
   const { outputs, logs } = await Bun.build({ entrypoints: [path], outdir: "build" });
   const { default: Page } = await import(outputs[0].path);
   logs.forEach(log => console.warn(log));
   console.log(Page());
-  return "";
+  return renderToString(Page());
 }
