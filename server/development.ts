b2e43c17fa34d0cbe6489a8a638d89f5306242bb
Lukas Bombach <lukas.bombach@googlemail.com>
2024-01-05 17:46:54 +0100

    compiler watch mode

--- server/development.ts
+++ server/development.ts
@@ -1,3 +1,4 @@
+import { watch } from "fs";
 import { Elysia } from "elysia";
 import { html } from "server/contenttype";
 import { log } from "server/log";
@@ -9,10 +10,15 @@ export async function startDevServer() {
     .onStart(async () => {
       log.blue("ready", "http://localhost:3000", "\n");
       await compileServerBundle("index");
-      log.green("compiled", "index");
+      log.green("compiled", "server/index");
     })
     .onResponse(({ path }) => log.blue(200, path))
     .get("/", () => render("index"))
     .get("/index.js", () => Bun.file("build/client/index.js"))
     .listen(3000);
 }
+
+watch("app", { recursive: true }, async () => {
+  await compileServerBundle("index");
+  log.green("compiled", "server/index");
+});
