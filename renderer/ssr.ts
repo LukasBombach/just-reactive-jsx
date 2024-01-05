742411d0c2d1ee5b64a7b98a5da1b66f41d9c089
Lukas Bombach <lukas.bombach@googlemail.com>
2024-01-05 09:55:32 +0100

    cleaner dev server with elysia

--- renderer/ssr.ts
+++ renderer/ssr.ts
@@ -1,9 +1,9 @@
 import { renderToString } from "../src/server/renderToString";
 
-export async function getHtmlResponse(path: string): Promise<string> {
+export async function getHtml(path: string): Promise<string> {
   const { outputs, logs } = await Bun.build({ entrypoints: [path], outdir: "build" });
   const { default: Page } = await import(outputs[0].path);
   logs.forEach(log => console.warn(log));
-  console.log(Page());
+  // console.log(Page());
   return renderToString(Page());
 }
--- server/development.ts
+++ server/development.ts
@@ -1,16 +1,16 @@
-import { getHtmlResponse } from "../renderer/getHtmlResponse";
+import c from "chalk";
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
+import { getHtml } from "renderer/ssr";
+
+// todo watch app dir for changes and recompile
+const ssr = await getHtml("app/index.tsx");
 
 export async function startDevServer() {
-  Bun.serve({
-    port: 3000,
-    development: true,
-    async fetch(req: Request) {
-      const html = await getHtmlResponse("app/index.tsx");
-      return new Response(html, {
-        headers: { "content-type": "text/html; charset=utf-8" },
-      });
-    },
-  });
-
-  console.log("\n🚀 http://localhost:3000\n");
+  new Elysia()
+    .use(html())
+    .onStart(() => console.log("\n 🚀 http://localhost:3000\n"))
+    .onResponse(({ path }) => console.log(" - ", c.blue(200), path))
+    .get("/", () => ssr)
+    .listen(3000);
 }
