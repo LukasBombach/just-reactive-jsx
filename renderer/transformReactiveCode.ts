d936122ea7c6aa7fa33057f25a55c709d8a4c401
Lukas Bombach <lukas.bombach@googlemail.com>
2024-01-07 22:11:41 +0100

    transformReactiveCode draft

--- renderer/transformReactiveCode.ts
+++ renderer/transformReactiveCode.ts
@@ -7,5 +7,13 @@ import { parse, print } from "@swc/core";
 export async function transformReactiveCode(input: string): Promise<string> {
   const program = await parse(input, { syntax: "typescript", tsx: true });
 
+  const variables = findEventHandlers(program).flatMap(findIndentifiers).flatMap(findDeclarators);
+  const usages = variables.flatMap(findUsages);
+  const assignments = variables.flatMap(findAssignments);
+
+  variables.forEach(replaceWithSignal);
+  usages.forEach(replaceWithGetters);
+  assignments.forEach(replaceWithSetters);
+
   return await print(program).then(o => o.code);
 }
