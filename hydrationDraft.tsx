c69e67207ee819e6f28973107173730837b21e90
Lukas Bombach <lukas.bombach@googlemail.com>
2024-01-29 08:56:22 +0100

    drafting

--- hydrationDraft.tsx
+++ hydrationDraft.tsx
@@ -2,18 +2,7 @@ import { signal, effect } from "@maverick-js/signals";
 import { domEffects } from "xx/runtime";
 
 // prettier-ignore
-const elements = [
-  document.querySelector("[r\\:0]")!.nextSibling as Text, // <!-- r:0 -->5<!-- r:0 -->
-  document.querySelector("[r\\:1]")!,                     // <button r:1>inc</button>
-] as const;
-
-// prettier-ignore
-const __ssr_state__ = [
-  5,                                                      // initial count state
-];
-
-// prettier-ignore
-const Counter = () => {
+const Counter = (_props: undefined, __ssr_state__: any[]) => {
   const count = signal(__ssr_state__[0]);
   return [
     { children: [count] },
@@ -21,9 +10,15 @@ const Counter = () => {
   ];
 };
 
-const count = signal(__ssr_state__[0]);
-const inc = () => count.set(count() + 1);
+// prettier-ignore
+const refs = [
+  document.querySelector("[r\\:0]")!.nextSibling as Text, // <!-- r:0 -->5<!-- r:0 -->
+  document.querySelector("[r\\:1]")!,                     // <button r:1>inc</button>
+] as const;
 
-effect(() => {
-  elements[0].textContent = String(count());
-});
+const hydraion = Counter(undefined, [5]);
+
+const CounterX = () => {
+  const count = signal(0);
+  return [{ children: [count] }, { onClick: () => count.set(count() + 1) }];
+};
