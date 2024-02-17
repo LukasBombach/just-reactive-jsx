e2bee606879c18a2b5495479ff36ed8a4662ce82
Lukas Bombach <lukas.bombach@googlemail.com>
2024-02-17 09:24:42 +0100

    ast-types get

--- recast/interaction.ts
+++ recast/interaction.ts
@@ -1,10 +1,14 @@
 import { parse, print } from "recast";
 import { visit } from "ast-types";
+import { NodePath as NodePathConstructor } from "ast-types";
 import type { namedTypes, ASTNode } from "ast-types";
 import type { NodePath } from "ast-types/node-path";
 
 const source = await Bun.file("app/Counter.tsx").text();
 const ast = parse(source);
+const path = new NodePathConstructor(ast);
+
+console.log(path.get("JSXAttribute"));
 
 function getEventHandlers(ast: ASTNode): NodePath<namedTypes.JSXAttribute, any>[] {
   const eventHandlers: NodePath<namedTypes.JSXAttribute, any>[] = [];
@@ -12,7 +16,11 @@ function getEventHandlers(ast: ASTNode): NodePath<namedTypes.JSXAttribute, any>[
   visit(ast, {
     visitJSXAttribute(path) {
       const name = typeof path.node.name.name === "string" ? path.node.name.name : path.node.name.name.name;
-      if (name.match(/^on[A-Z]/)) eventHandlers.push(path);
+      if (name.match(/^on[A-Z]/)) {
+        eventHandlers.push(path);
+        console.log(path.scope.getBindings());
+      }
+
       return false;
     },
   });
@@ -20,4 +28,4 @@ function getEventHandlers(ast: ASTNode): NodePath<namedTypes.JSXAttribute, any>[
   return eventHandlers;
 }
 
-console.log(getEventHandlers(ast).map(path => print(path.node).code));
+//console.log(getEventHandlers(ast).map(path => print(path.node).code));
